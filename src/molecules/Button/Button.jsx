129fcb391b8a97711f391a2aab6b20ed3e30b9b9
codechappie <davidproyecto01@gmail.com>
2020-07-18 14:05:38 -0500

    updated/(Button): Improving component - codechappie

--- src/molecules/Button/Button.jsx
+++ src/molecules/Button/Button.jsx
@@ -4,18 +4,9 @@ import React from 'react';
 import './Button.scss';
 import Icon from 'atoms/Icon/Icon';
 
-export const ButtonPrimary = ({ text, src, name, classes, size }) => {
+export const Button = ({ text, type, src, name, classes, size }) => {
   return (
-    <div className="btnPrimary ">
-      {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
-      <span>{text}</span>
-    </div>
-  );
-};
-
-export const ButtonOutlinePrimary = ({ text, src, name, classes, size }) => {
-  return (
-    <div className="btnOutlinePrimary ">
+    <div className={type}>
       {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
       <span>{text}</span>
     </div>
