194b3640fecfb84d46fb3c0e6fcdcfd0f3b31be4
codechappie <davidproyecto01@gmail.com>
2020-07-18 13:49:01 -0500

    updated/(Button): Improving component - codechappie

--- src/molecules/Button/Button.jsx
+++ src/molecules/Button/Button.jsx
@@ -2,18 +2,21 @@
 
 import React from 'react';
 import './Button.scss';
+import Icon from 'atoms/Icon/Icon';
 
-export const BtnPrimary = ({ text }) => {
+export const BtnPrimary = ({ text, src, name, classes, size }) => {
   return (
-    <div className="btnPrimary">
+    <div className="btnPrimary ">
+      {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
       <span>{text}</span>
     </div>
   );
 };
 
-export const BtnOutlinePrimary = ({ text }) => {
+export const BtnOutlinePrimary = ({ text, src, name, classes, size }) => {
   return (
-    <div className="btnOutlinePrimary">
+    <div className="btnOutlinePrimary ">
+      {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
       <span>{text}</span>
     </div>
   );
--- src/molecules/Button/Button.md
+++ src/molecules/Button/Button.md
@@ -1,4 +1,15 @@
 # Button molecule
-Description
+
+Button element return a primary or primary outline button.
+
 ## Props
-text
+
+[text](#1-text) [src](#2-src) [name](#3-name) [classes](#4-classes) [size](#5-size)
+
+#### 1. text
+
+This prop receives the text which you want to add to the button.
+
+For more information about use an icon in buton check the following link:
+
+[src, name, classes, size](../../atoms/Icon/Icon.md)
