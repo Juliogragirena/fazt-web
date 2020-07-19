aeb4069e691fafde371fd53cabffa410659ec0b9
Willishakespeare <Skr13@outlook.com>
2020-07-19 02:45:23 -0700

    refactor(icon): refactor to use customs SVG

--- src/atoms/Icon/Icon.jsx
+++ src/atoms/Icon/Icon.jsx
@@ -1,11 +1,257 @@
 // Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
 import React from 'react';
+import { ReactComponent as Arrow } from '@assets/images/icons/web/arrow.svg';
+import { ReactComponent as Close } from '@assets/images/icons/web/close.svg';
+import { ReactComponent as Config } from '@assets/images/icons/web/config.svg';
+import { ReactComponent as Facebook } from '@assets/images/icons/web/facebook.svg';
+import { ReactComponent as Faztlogo } from '@assets/images/icons/web/faztlogo.svg';
+import { ReactComponent as Git } from '@assets/images/icons/web/git.svg';
+import { ReactComponent as Github } from '@assets/images/icons/web/github.svg';
+import { ReactComponent as Gitlab } from '@assets/images/icons/web/gitlab.svg';
+import { ReactComponent as Google } from '@assets/images/icons/web/google.svg';
+import { ReactComponent as Heart } from '@assets/images/icons/web/heart.svg';
+import { ReactComponent as Instagram } from '@assets/images/icons/web/instagram.svg';
+import { ReactComponent as Link } from '@assets/images/icons/web/link.svg';
+import { ReactComponent as Linkedin } from '@assets/images/icons/web/linkedin.svg';
+import { ReactComponent as Twitter } from '@assets/images/icons/web/twitter.svg';
 
-export default ({ src, name, classes = '', size = '' }) => {
-  if (!name) {
-    return <img className={`icon ${classes}`} src={src} alt="icon" />;
-  } else {
-    return <i className={`fas fa-${name} ${size} ${classes} icon `} />;
-  }
-};
+import { ReactComponent as amazonwebservices } from '@assets/images/icons/code/amazonwebservices.svg';
+import { ReactComponent as angularjs } from '@assets/images/icons/code/angularjs.svg';
+import { ReactComponent as apache } from '@assets/images/icons/code/apache.svg';
+import { ReactComponent as appcelerator } from '@assets/images/icons/code/appcelerator.svg';
+import { ReactComponent as atom } from '@assets/images/icons/code/atom.svg';
+import { ReactComponent as babel } from '@assets/images/icons/code/babel.svg';
+import { ReactComponent as backbonejs } from '@assets/images/icons/code/backbonejs.svg';
+import { ReactComponent as behance } from '@assets/images/icons/code/behance.svg';
+import { ReactComponent as bitbucket } from '@assets/images/icons/code/bitbucket.svg';
+import { ReactComponent as bootstrap } from '@assets/images/icons/code/bootstrap.svg';
+import { ReactComponent as bower } from '@assets/images/icons/code/bower.svg';
+import { ReactComponent as c } from '@assets/images/icons/code/c.svg';
+import { ReactComponent as cakephp } from '@assets/images/icons/code/cakephp.svg';
+import { ReactComponent as ceylon } from '@assets/images/icons/code/ceylon.svg';
+import { ReactComponent as clojure } from '@assets/images/icons/code/clojure.svg';
+import { ReactComponent as clojurescript } from '@assets/images/icons/code/clojurescript.svg';
+import { ReactComponent as codeigniter } from '@assets/images/icons/code/codeigniter.svg';
+import { ReactComponent as coffeescript } from '@assets/images/icons/code/coffeescript.svg';
+import { ReactComponent as confluence } from '@assets/images/icons/code/confluence.svg';
+import { ReactComponent as couchdb } from '@assets/images/icons/code/couchdb.svg';
+import { ReactComponent as cplusplus } from '@assets/images/icons/code/cplusplus.svg';
+import { ReactComponent as csharp } from '@assets/images/icons/code/csharp.svg';
+import { ReactComponent as css3 } from '@assets/images/icons/code/css3.svg';
+import { ReactComponent as cucumber } from '@assets/images/icons/code/cucumber.svg';
+import { ReactComponent as d3js } from '@assets/images/icons/code/d3js.svg';
+import { ReactComponent as django } from '@assets/images/icons/code/django.svg';
+import { ReactComponent as docker } from '@assets/images/icons/code/docker.svg';
+import { ReactComponent as doctrine } from '@assets/images/icons/code/doctrine.svg';
+import { ReactComponent as dotnet } from '@assets/images/icons/code/dotnet.svg';
+import { ReactComponent as drupal } from '@assets/images/icons/code/drupal.svg';
+import { ReactComponent as electron } from '@assets/images/icons/code/electron.svg';
+import { ReactComponent as elm } from '@assets/images/icons/code/elm.svg';
+import { ReactComponent as ember } from '@assets/images/icons/code/ember.svg';
+import { ReactComponent as erlang } from '@assets/images/icons/code/erlang.svg';
+import { ReactComponent as express } from '@assets/images/icons/code/express.svg';
+import { ReactComponent as foundation } from '@assets/images/icons/code/foundation.svg';
+import { ReactComponent as gatling } from '@assets/images/icons/code/gatling.svg';
+import { ReactComponent as gimp } from '@assets/images/icons/code/gimp.svg';
+import { ReactComponent as go } from '@assets/images/icons/code/go.svg';
+import { ReactComponent as gradle } from '@assets/images/icons/code/gradle.svg';
+import { ReactComponent as grails } from '@assets/images/icons/code/grails.svg';
+import { ReactComponent as groovy } from '@assets/images/icons/code/groovy.svg';
+import { ReactComponent as grunt } from '@assets/images/icons/code/grunt.svg';
+import { ReactComponent as gulp } from '@assets/images/icons/code/gulp.svg';
+import { ReactComponent as handlebars } from '@assets/images/icons/code/handlebars.svg';
+import { ReactComponent as heroku } from '@assets/images/icons/code/heroku.svg';
+import { ReactComponent as html5 } from '@assets/images/icons/code/html5.svg';
+import { ReactComponent as illustrator } from '@assets/images/icons/code/illustrator.svg';
+import { ReactComponent as inkscape } from '@assets/images/icons/code/inkscape.svg';
+import { ReactComponent as intellij } from '@assets/images/icons/code/intellij.svg';
+import { ReactComponent as ionic } from '@assets/images/icons/code/ionic.svg';
+import { ReactComponent as jasmine } from '@assets/images/icons/code/jasmine.svg';
+import { ReactComponent as java } from '@assets/images/icons/code/java.svg';
+import { ReactComponent as javascript } from '@assets/images/icons/code/javascript.svg';
+import { ReactComponent as jeet } from '@assets/images/icons/code/jeet.svg';
+import { ReactComponent as jetbrains } from '@assets/images/icons/code/jetbrains.svg';
+import { ReactComponent as jquery } from '@assets/images/icons/code/jquery.svg';
+import { ReactComponent as krakenjs } from '@assets/images/icons/code/krakenjs.svg';
+import { ReactComponent as laravel } from '@assets/images/icons/code/laravel.svg';
+import { ReactComponent as less } from '@assets/images/icons/code/less.svg';
+import { ReactComponent as meteor } from '@assets/images/icons/code/meteor.svg';
+import { ReactComponent as mongodb } from '@assets/images/icons/code/mongodb.svg';
+import { ReactComponent as moodle } from '@assets/images/icons/code/moodle.svg';
+import { ReactComponent as mysql } from '@assets/images/icons/code/mysql.svg';
+import { ReactComponent as nginx } from '@assets/images/icons/code/nginx.svg';
+import { ReactComponent as nodejs } from '@assets/images/icons/code/nodejs.svg';
+import { ReactComponent as nodewebkit } from '@assets/images/icons/code/nodewebkit.svg';
+import { ReactComponent as notfound } from '@assets/images/icons/code/notfound.svg';
+import { ReactComponent as npm } from '@assets/images/icons/code/npm.svg';
+import { ReactComponent as oracle } from '@assets/images/icons/code/oracle.svg';
+import { ReactComponent as photoshop } from '@assets/images/icons/code/photoshop.svg';
+import { ReactComponent as php } from '@assets/images/icons/code/php.svg';
+import { ReactComponent as phpstorm } from '@assets/images/icons/code/phpstorm.svg';
+import { ReactComponent as postgresql } from '@assets/images/icons/code/postgresql.svg';
+import { ReactComponent as protractor } from '@assets/images/icons/code/protractor.svg';
+import { ReactComponent as pycharm } from '@assets/images/icons/code/pycharm.svg';
+import { ReactComponent as python } from '@assets/images/icons/code/python.svg';
+import { ReactComponent as rails } from '@assets/images/icons/code/rails.svg';
+import { ReactComponent as react } from '@assets/images/icons/code/react.svg';
+import { ReactComponent as redis } from '@assets/images/icons/code/redis.svg';
+import { ReactComponent as redux } from '@assets/images/icons/code/redux.svg';
+import { ReactComponent as ruby } from '@assets/images/icons/code/ruby.svg';
+import { ReactComponent as rubymine } from '@assets/images/icons/code/rubymine.svg';
+import { ReactComponent as rust } from '@assets/images/icons/code/rust.svg';
+import { ReactComponent as sass } from '@assets/images/icons/code/sass.svg';
+import { ReactComponent as scala } from '@assets/images/icons/code/scala.svg';
+import { ReactComponent as sequelize } from '@assets/images/icons/code/sequelize.svg';
+import { ReactComponent as sketch } from '@assets/images/icons/code/sketch.svg';
+import { ReactComponent as slack } from '@assets/images/icons/code/slack.svg';
+import { ReactComponent as ssh } from '@assets/images/icons/code/ssh.svg';
+import { ReactComponent as stylus } from '@assets/images/icons/code/stylus.svg';
+import { ReactComponent as swift } from '@assets/images/icons/code/swift.svg';
+import { ReactComponent as symfony } from '@assets/images/icons/code/symfony.svg';
+import { ReactComponent as tomcat } from '@assets/images/icons/code/tomcat.svg';
+import { ReactComponent as travis } from '@assets/images/icons/code/travis.svg';
+import { ReactComponent as typescript } from '@assets/images/icons/code/typescript.svg';
+import { ReactComponent as vagrant } from '@assets/images/icons/code/vagrant.svg';
+import { ReactComponent as vim } from '@assets/images/icons/code/vim.svg';
+import { ReactComponent as vuejs } from '@assets/images/icons/code/vuejs.svg';
+import { ReactComponent as webpack } from '@assets/images/icons/code/webpack.svg';
+import { ReactComponent as yarn } from '@assets/images/icons/code/yarn.svg';
+import { ReactComponent as yii } from '@assets/images/icons/code/yii.svg';
+import { ReactComponent as zend } from '@assets/images/icons/code/zend.svg';
+function IconWeb({ name, height, width }) {
+  const IconList = [
+    { name: 'arrow', icon: Arrow },
+    { name: 'close', icon: Close },
+    { name: 'config', icon: Config },
+    { name: 'facebook', icon: Facebook },
+    { name: 'faztlogo', icon: Faztlogo },
+    { name: 'git', icon: Git },
+    { name: 'github', icon: Github },
+    { name: 'gitlab', icon: Gitlab },
+    { name: 'google', icon: Google },
+    { name: 'heart', icon: Heart },
+    { name: 'instagram', icon: Instagram },
+    { name: 'link', icon: Link },
+    { name: 'linkedin', icon: Linkedin },
+    { name: 'twitter', icon: Twitter }
+  ];
+
+  const findIcon = IconList.find((e) => e.name === name);
+
+  return <>{findIcon ? <findIcon.icon height={height} width={width} /> : <></>}</>;
+}
+
+function IconCode({ name, height, width }) {
+  const IconList = [
+    { name: 'amazonwebservices', icon: amazonwebservices },
+    { name: 'angularjs', icon: angularjs },
+    { name: 'apache', icon: apache },
+    { name: 'appcelerator', icon: appcelerator },
+    { name: 'atom', icon: atom },
+    { name: 'babel', icon: babel },
+    { name: 'backbonejs', icon: backbonejs },
+    { name: 'behance', icon: behance },
+    { name: 'bitbucket', icon: bitbucket },
+    { name: 'bootstrap', icon: bootstrap },
+    { name: 'bower', icon: bower },
+    { name: 'c', icon: c },
+    { name: 'cakephp', icon: cakephp },
+    { name: 'ceylon', icon: ceylon },
+    { name: 'clojure', icon: clojure },
+    { name: 'clojurescript', icon: clojurescript },
+    { name: 'codeigniter', icon: codeigniter },
+    { name: 'coffeescript', icon: coffeescript },
+    { name: 'confluence', icon: confluence },
+    { name: 'couchdb', icon: couchdb },
+    { name: 'cplusplus', icon: cplusplus },
+    { name: 'csharp', icon: csharp },
+    { name: 'css3', icon: css3 },
+    { name: 'cucumber', icon: cucumber },
+    { name: 'd3js', icon: d3js },
+    { name: 'django', icon: django },
+    { name: 'docker', icon: docker },
+    { name: 'doctrine', icon: doctrine },
+    { name: 'dotnet', icon: dotnet },
+    { name: 'drupal', icon: drupal },
+    { name: 'electron', icon: electron },
+    { name: 'elm', icon: elm },
+    { name: 'ember', icon: ember },
+    { name: 'erlang', icon: erlang },
+    { name: 'express', icon: express },
+    { name: 'foundation', icon: foundation },
+    { name: 'gatling', icon: gatling },
+    { name: 'gimp', icon: gimp },
+    { name: 'go', icon: go },
+    { name: 'gradle', icon: gradle },
+    { name: 'grails', icon: grails },
+    { name: 'groovy', icon: groovy },
+    { name: 'grunt', icon: grunt },
+    { name: 'gulp', icon: gulp },
+    { name: 'handlebars', icon: handlebars },
+    { name: 'heroku', icon: heroku },
+    { name: 'html5', icon: html5 },
+    { name: 'illustrator', icon: illustrator },
+    { name: 'inkscape', icon: inkscape },
+    { name: 'intellij', icon: intellij },
+    { name: 'ionic', icon: ionic },
+    { name: 'jasmine', icon: jasmine },
+    { name: 'java', icon: java },
+    { name: 'javascript', icon: javascript },
+    { name: 'jeet', icon: jeet },
+    { name: 'jetbrains', icon: jetbrains },
+    { name: 'jquery', icon: jquery },
+    { name: 'krakenjs', icon: krakenjs },
+    { name: 'laravel', icon: laravel },
+    { name: 'less', icon: less },
+    { name: 'meteor', icon: meteor },
+    { name: 'mongodb', icon: mongodb },
+    { name: 'moodle', icon: moodle },
+    { name: 'mysql', icon: mysql },
+    { name: 'nginx', icon: nginx },
+    { name: 'nodejs', icon: nodejs },
+    { name: 'nodewebkit', icon: nodewebkit },
+    { name: 'notfound', icon: notfound },
+    { name: 'npm', icon: npm },
+    { name: 'oracle', icon: oracle },
+    { name: 'photoshop', icon: photoshop },
+    { name: 'php', icon: php },
+    { name: 'phpstorm', icon: phpstorm },
+    { name: 'postgresql', icon: postgresql },
+    { name: 'protractor', icon: protractor },
+    { name: 'pycharm', icon: pycharm },
+    { name: 'python', icon: python },
+    { name: 'rails', icon: rails },
+    { name: 'react', icon: react },
+    { name: 'redis', icon: redis },
+    { name: 'redux', icon: redux },
+    { name: 'ruby', icon: ruby },
+    { name: 'rubymine', icon: rubymine },
+    { name: 'rust', icon: rust },
+    { name: 'sass', icon: sass },
+    { name: 'scala', icon: scala },
+    { name: 'sequelize', icon: sequelize },
+    { name: 'sketch', icon: sketch },
+    { name: 'slack', icon: slack },
+    { name: 'ssh', icon: ssh },
+    { name: 'stylus', icon: stylus },
+    { name: 'swift', icon: swift },
+    { name: 'symfony', icon: symfony },
+    { name: 'tomcat', icon: tomcat },
+    { name: 'travis', icon: travis },
+    { name: 'typescript', icon: typescript },
+    { name: 'vagrant', icon: vagrant },
+    { name: 'vim', icon: vim },
+    { name: 'vuejs', icon: vuejs },
+    { name: 'webpack', icon: webpack },
+    { name: 'yarn', icon: yarn },
+    { name: 'yii', icon: yii },
+    { name: 'zend', icon: zend }
+  ];
+
+  const findIcon = IconList.find((e) => e.name === name);
+
+  return <>{findIcon ? <findIcon.icon height={height} width={width} /> : <></>}</>;
+}
+
+export { IconWeb, IconCode };
--- src/atoms/Icon/index.jsx
+++ src/atoms/Icon/index.jsx
@@ -1,6 +1,6 @@
 // Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
-import Icon from './Icon';
+import { IconWeb, IconCode } from './Icon';
 import './Icon.scss';
 
-export default Icon;
+export { IconWeb, IconCode };
