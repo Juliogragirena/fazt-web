71f289d0a164321e35ca6c457db6be90b8549250
Leave20 <thetai6102@gmail.com>
2020-07-18 19:24:24 -0500

    updated/(Button): Improving component - codechappie
    cherry change

--- src/molecules/Button/Button.jsx
+++ src/molecules/Button/Button.jsx
@@ -4,9 +4,18 @@ import React from 'react';
 import './Button.scss';
 import Icon from 'atoms/Icon/Icon';
 
-export const Button = ({ text, type, src, name, classes, size }) => {
+export const BtnPrimary = ({ text, src, name, classes, size }) => {
   return (
-    <div className={type}>
+    <div className="btnPrimary ">
+      {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
+      <span>{text}</span>
+    </div>
+  );
+};
+
+export const BtnOutlinePrimary = ({ text, src, name, classes, size }) => {
+  return (
+    <div className="btnOutlinePrimary ">
       {(name || src) && <Icon src={src} name={name} size={size} classes={classes} />}
       <span>{text}</span>
     </div>
--- src/molecules/Button/Button.md
+++ src/molecules/Button/Button.md
@@ -4,15 +4,11 @@ Button element return a primary or primary outline button.
 
 ## Props
 
-[text](#1-text) [type](#2-type) [name](#3-name) [classes](#4-classes) [size](#5-size) [src](#6-src)
+[text](#1-text) [src](#2-src) [name](#3-name) [classes](#4-classes) [size](#5-size)
 
 #### 1. text
 
 This prop receives the text which you want to add to the button.
-
-#### 2. type
-
-This prop receives one of the next values: 'primary' or 'outline' which indicate the style of the button
 
 For more information about use an icon in buton check the following link:
 
