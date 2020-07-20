bcefb375d6e6c78da3a012b4107f0f0c0fab00f3
Jean Pierre Ortiz <45444014+CoffeJeanCode@users.noreply.github.com>
2020-07-19 19:41:29 -0500

    update(component): navigation

--- src/templates/Main/Main.jsx
+++ src/templates/Main/Main.jsx
@@ -1,12 +1,12 @@
 // Copyright 2020 Fazt Community ~ All rights reserved. MIT license.
 
 import React from 'react';
-import Navigation from '@organism/Navigation/index';
+import Navigation from '@organism/Navigation/';
 
 function Main() {
   return (
     <div>
-      <Navigation type="light"></Navigation>
+      <Navigation type="light" />
       Landing Page
     </div>
   );
