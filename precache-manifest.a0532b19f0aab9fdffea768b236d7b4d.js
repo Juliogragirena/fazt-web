36f8a80935a5c1b62c9d7702d5cff093e01cef2c
Fedeya <elfedeminaya@gmail.com>
2020-07-22 21:19:36 -0300

    Updates

---
+++ precache-manifest.a0532b19f0aab9fdffea768b236d7b4d.js
@@ -0,0 +1,626 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f982606ccffe6ddd79482824a892ed",
+    "url": "/fazt-web/index.html"
+  },
+  {
+    "revision": "5ea121c3e445ce989a7b",
+    "url": "/fazt-web/static/css/main.32a5c6eb.chunk.css"
+  },
+  {
+    "revision": "9e80e09a6f3bfd528100",
+    "url": "/fazt-web/static/js/2.d2ed398b.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/fazt-web/static/js/2.d2ed398b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea121c3e445ce989a7b",
+    "url": "/fazt-web/static/js/main.59caa69a.chunk.js"
+  },
+  {
+    "revision": "c7f7847534d6567fcd1f",
+    "url": "/fazt-web/static/js/runtime-main.2c42768a.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/fazt-web/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "fb7e3a294cb07a54605a8bb27f0cd528",
+    "url": "/fazt-web/static/media/OpenSans-ExtraBold.fb7e3a29.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/fazt-web/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/fazt-web/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/fazt-web/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/fazt-web/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/fazt-web/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/fazt-web/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/fazt-web/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/fazt-web/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/fazt-web/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "0589ea93433b4850338f4ba8fd10ed2c",
+    "url": "/fazt-web/static/media/amazonwebservices.0589ea93.svg"
+  },
+  {
+    "revision": "db12f97bda8cd0682f2c1daa1396daa3",
+    "url": "/fazt-web/static/media/angularjs.db12f97b.svg"
+  },
+  {
+    "revision": "4b91caa2c29de1de4cf35620a529577a",
+    "url": "/fazt-web/static/media/apache.4b91caa2.svg"
+  },
+  {
+    "revision": "b1c490a45cea6fe3f57fafe1062c10bc",
+    "url": "/fazt-web/static/media/appcelerator.b1c490a4.svg"
+  },
+  {
+    "revision": "85656b60232aadccb0b5ed04a4ea5831",
+    "url": "/fazt-web/static/media/arrow.85656b60.svg"
+  },
+  {
+    "revision": "f3402f5b60583e290c1897f57b72401e",
+    "url": "/fazt-web/static/media/arrowup.f3402f5b.svg"
+  },
+  {
+    "revision": "7ce634bb90809b8d4eb25a19b3fc5e9d",
+    "url": "/fazt-web/static/media/atom.7ce634bb.svg"
+  },
+  {
+    "revision": "529bf9cdff934f9ce5f709e3c2e9073d",
+    "url": "/fazt-web/static/media/babel.529bf9cd.svg"
+  },
+  {
+    "revision": "eeea4110bbea94a386741e9d2a550032",
+    "url": "/fazt-web/static/media/backbonejs.eeea4110.svg"
+  },
+  {
+    "revision": "e73f76fdc4922dfc04ffbca0c8dc2b0d",
+    "url": "/fazt-web/static/media/behance.e73f76fd.svg"
+  },
+  {
+    "revision": "a3794918b2aa72c69a55027ea08486a0",
+    "url": "/fazt-web/static/media/bitbucket.a3794918.svg"
+  },
+  {
+    "revision": "76d7c6aabe5de63af067918dc270986c",
+    "url": "/fazt-web/static/media/bootstrap.76d7c6aa.svg"
+  },
+  {
+    "revision": "8ca3d10ca200b1626d44db1ec826fa21",
+    "url": "/fazt-web/static/media/bower.8ca3d10c.svg"
+  },
+  {
+    "revision": "3505d536922da9f20d0ce457cfb7775a",
+    "url": "/fazt-web/static/media/c.3505d536.svg"
+  },
+  {
+    "revision": "1effe9aa339eb9461941ac8322e4f3ba",
+    "url": "/fazt-web/static/media/cakephp.1effe9aa.svg"
+  },
+  {
+    "revision": "1d83444c3f0edc4e9b65bda74a722e5d",
+    "url": "/fazt-web/static/media/ceylon.1d83444c.svg"
+  },
+  {
+    "revision": "564f474fba22eb0fa9b67da2d3a4b701",
+    "url": "/fazt-web/static/media/clojure.564f474f.svg"
+  },
+  {
+    "revision": "7724577f5b7b68d8a2559ca5d4870038",
+    "url": "/fazt-web/static/media/clojurescript.7724577f.svg"
+  },
+  {
+    "revision": "b30e9ef0b90cd7574fcf6da56de082aa",
+    "url": "/fazt-web/static/media/close.b30e9ef0.svg"
+  },
+  {
+    "revision": "420dde8f99825be6b2d2fd91092bc286",
+    "url": "/fazt-web/static/media/codeigniter.420dde8f.svg"
+  },
+  {
+    "revision": "d86ec9738095aedc8b9167e778bfbd82",
+    "url": "/fazt-web/static/media/coffeescript.d86ec973.svg"
+  },
+  {
+    "revision": "10bf31908cae114c843df234f4befbbe",
+    "url": "/fazt-web/static/media/config.10bf3190.svg"
+  },
+  {
+    "revision": "adee243a8cef06427e2261e05a1957a0",
+    "url": "/fazt-web/static/media/confluence.adee243a.svg"
+  },
+  {
+    "revision": "9deb93976aec63450efd04b3e97d655e",
+    "url": "/fazt-web/static/media/couchdb.9deb9397.svg"
+  },
+  {
+    "revision": "a5d9bc3d587f95ced9864a8481979bcd",
+    "url": "/fazt-web/static/media/cplusplus.a5d9bc3d.svg"
+  },
+  {
+    "revision": "c6961b79062b431cb39106d60635832d",
+    "url": "/fazt-web/static/media/csharp.c6961b79.svg"
+  },
+  {
+    "revision": "a70a7e3e545f3ddc5707d4c4da8cfa17",
+    "url": "/fazt-web/static/media/css3.a70a7e3e.svg"
+  },
+  {
+    "revision": "fb904a5b65c16ce783b1d59a0ea46618",
+    "url": "/fazt-web/static/media/cucumber.fb904a5b.svg"
+  },
+  {
+    "revision": "bd70f32d9ff43e4ee3be96607cb6a210",
+    "url": "/fazt-web/static/media/d3js.bd70f32d.svg"
+  },
+  {
+    "revision": "502a71b352dfa3e7fcb82770b5549be3",
+    "url": "/fazt-web/static/media/discord.502a71b3.svg"
+  },
+  {
+    "revision": "792f835aac1501f579814406af2e0eae",
+    "url": "/fazt-web/static/media/django.792f835a.svg"
+  },
+  {
+    "revision": "86726983eb6e4a316184acdcf158228e",
+    "url": "/fazt-web/static/media/docker.86726983.svg"
+  },
+  {
+    "revision": "a6affd62470132e0b1e497c47209bec7",
+    "url": "/fazt-web/static/media/doctrine.a6affd62.svg"
+  },
+  {
+    "revision": "ce282a06aa2f559bfd1f00ebfc2bbf88",
+    "url": "/fazt-web/static/media/dotnet.ce282a06.svg"
+  },
+  {
+    "revision": "2895720c74d0d70964c8bdcc4b969032",
+    "url": "/fazt-web/static/media/drupal.2895720c.svg"
+  },
+  {
+    "revision": "e903b609335052437cac1ae5403160e9",
+    "url": "/fazt-web/static/media/electron.e903b609.svg"
+  },
+  {
+    "revision": "11544098ff3399a9f156291621c60017",
+    "url": "/fazt-web/static/media/elm.11544098.svg"
+  },
+  {
+    "revision": "eca96133d1ca24712d1255cc7bd22f08",
+    "url": "/fazt-web/static/media/ember.eca96133.svg"
+  },
+  {
+    "revision": "3a89b2fb93aee2e99efd8eb9f4431b25",
+    "url": "/fazt-web/static/media/erlang.3a89b2fb.svg"
+  },
+  {
+    "revision": "f1a0b0e9c7eb8a3294e84f11441de5df",
+    "url": "/fazt-web/static/media/express.f1a0b0e9.svg"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/fazt-web/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/fazt-web/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/fazt-web/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/fazt-web/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/fazt-web/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/fazt-web/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/fazt-web/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/fazt-web/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/fazt-web/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/fazt-web/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/fazt-web/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/fazt-web/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/fazt-web/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/fazt-web/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/fazt-web/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "fe91aad7c45be742926fde417c11172f",
+    "url": "/fazt-web/static/media/facebook.fe91aad7.svg"
+  },
+  {
+    "revision": "4c8392b78843c278d9ebdbd90526878d",
+    "url": "/fazt-web/static/media/faztCommunityIcon.4c8392b7.svg"
+  },
+  {
+    "revision": "9a9287cdbf89f4fca0f3ae3486b1695a",
+    "url": "/fazt-web/static/media/faztcommunity.9a9287cd.svg"
+  },
+  {
+    "revision": "0063d6399a15d0dd6f1cbff6d9ce5048",
+    "url": "/fazt-web/static/media/faztlogo.0063d639.svg"
+  },
+  {
+    "revision": "d4022cbd5b9d1167bb8b49711b2f6d15",
+    "url": "/fazt-web/static/media/foundation.d4022cbd.svg"
+  },
+  {
+    "revision": "8e6794aecf534d75b8f86c3b3a2efcee",
+    "url": "/fazt-web/static/media/gatling.8e6794ae.svg"
+  },
+  {
+    "revision": "eae82bebf5f3d96f3872e71ac41fec9c",
+    "url": "/fazt-web/static/media/gimp.eae82beb.svg"
+  },
+  {
+    "revision": "1915e0bdfac748a30adbb093df6fea15",
+    "url": "/fazt-web/static/media/git.1915e0bd.svg"
+  },
+  {
+    "revision": "0463cff5f19971f1672a7cb7e56a1e50",
+    "url": "/fazt-web/static/media/github.0463cff5.svg"
+  },
+  {
+    "revision": "b4bfb687148c95c8835343780223a647",
+    "url": "/fazt-web/static/media/gitlab.b4bfb687.svg"
+  },
+  {
+    "revision": "74c1374cfabfb69f55a5377482ab5852",
+    "url": "/fazt-web/static/media/go.74c1374c.svg"
+  },
+  {
+    "revision": "1947e406a49e9772e9c7c439b37db31a",
+    "url": "/fazt-web/static/media/google.1947e406.svg"
+  },
+  {
+    "revision": "fcbe9c1b60048b2550ba88a8122c6510",
+    "url": "/fazt-web/static/media/gradle.fcbe9c1b.svg"
+  },
+  {
+    "revision": "eaebbed4551f8e1757e4c46069165a29",
+    "url": "/fazt-web/static/media/grails.eaebbed4.svg"
+  },
+  {
+    "revision": "f1e1f2b131354b71b6dd228291c5004d",
+    "url": "/fazt-web/static/media/groovy.f1e1f2b1.svg"
+  },
+  {
+    "revision": "8682255d70248c817e80d4393706433d",
+    "url": "/fazt-web/static/media/grunt.8682255d.svg"
+  },
+  {
+    "revision": "3b309d8dc61a691b3ed514f62c2f8bcc",
+    "url": "/fazt-web/static/media/gulp.3b309d8d.svg"
+  },
+  {
+    "revision": "6954cf00013373c5684ff940cf9d70e3",
+    "url": "/fazt-web/static/media/handlebars.6954cf00.svg"
+  },
+  {
+    "revision": "07c2317559fb5084234574e0138cf54f",
+    "url": "/fazt-web/static/media/heart.07c23175.svg"
+  },
+  {
+    "revision": "3af582e56c213a6439e38c3f360bd633",
+    "url": "/fazt-web/static/media/heroku.3af582e5.svg"
+  },
+  {
+    "revision": "eb0bfe64ddcaa0b4c3be19b19541b584",
+    "url": "/fazt-web/static/media/html5.eb0bfe64.svg"
+  },
+  {
+    "revision": "175cfa63af9e184f41b33b0f2c8cf807",
+    "url": "/fazt-web/static/media/illustrator.175cfa63.svg"
+  },
+  {
+    "revision": "007eef57d15833b732b28096fa378ec8",
+    "url": "/fazt-web/static/media/inkscape.007eef57.svg"
+  },
+  {
+    "revision": "d540e0b7ce2090ac57987e6d2a244148",
+    "url": "/fazt-web/static/media/instagram.d540e0b7.svg"
+  },
+  {
+    "revision": "357626e4c1d133bc4185c3eab5849d91",
+    "url": "/fazt-web/static/media/intellij.357626e4.svg"
+  },
+  {
+    "revision": "241f2cea8366ed66e2d161019d06a147",
+    "url": "/fazt-web/static/media/ionic.241f2cea.svg"
+  },
+  {
+    "revision": "8482149cb242b37624b777f7c626ce18",
+    "url": "/fazt-web/static/media/jasmine.8482149c.svg"
+  },
+  {
+    "revision": "e6d87e2dca08c953fe3acd1275ed4f4e",
+    "url": "/fazt-web/static/media/java.e6d87e2d.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/fazt-web/static/media/javascript.de79c8f1.svg"
+  },
+  {
+    "revision": "75db59a97f36be54ae36399a2f64becf",
+    "url": "/fazt-web/static/media/jeet.75db59a9.svg"
+  },
+  {
+    "revision": "3a8572b347f48b346c867d1fa4c7a712",
+    "url": "/fazt-web/static/media/jetbrains.3a8572b3.svg"
+  },
+  {
+    "revision": "8ec9bacf8bc78b8ad161b5e9a2a16e87",
+    "url": "/fazt-web/static/media/jquery.8ec9bacf.svg"
+  },
+  {
+    "revision": "23a1733974f3e4e989d636a82a45b258",
+    "url": "/fazt-web/static/media/krakenjs.23a17339.svg"
+  },
+  {
+    "revision": "14fd8fbfa002c2808342029305597db0",
+    "url": "/fazt-web/static/media/laravel.14fd8fbf.svg"
+  },
+  {
+    "revision": "b3dea9988573d89b8da0d00acc3ed9f1",
+    "url": "/fazt-web/static/media/less.b3dea998.svg"
+  },
+  {
+    "revision": "8f3c4901c23f6fc4f7c3e09dad797ede",
+    "url": "/fazt-web/static/media/link.8f3c4901.svg"
+  },
+  {
+    "revision": "2950f27064949a2f455708014ca0702e",
+    "url": "/fazt-web/static/media/linkedin.2950f270.svg"
+  },
+  {
+    "revision": "8b80b44884336370dcc6b68114f5d963",
+    "url": "/fazt-web/static/media/menu-icon.8b80b448.svg"
+  },
+  {
+    "revision": "2e4ff7ccdef83357125cfeb11eafd420",
+    "url": "/fazt-web/static/media/menuicon.2e4ff7cc.svg"
+  },
+  {
+    "revision": "5b0205da9b32941104b606c56c83ee91",
+    "url": "/fazt-web/static/media/meteor.5b0205da.svg"
+  },
+  {
+    "revision": "b1847af9e7cf00cbe2d44e9fa173d281",
+    "url": "/fazt-web/static/media/mongodb.b1847af9.svg"
+  },
+  {
+    "revision": "0763f2492d17c139e43c5bcea423d9c7",
+    "url": "/fazt-web/static/media/moodle.0763f249.svg"
+  },
+  {
+    "revision": "c02ade79e7034732a856b802471a5f31",
+    "url": "/fazt-web/static/media/mysql.c02ade79.svg"
+  },
+  {
+    "revision": "9bb21db07e19c2a7a5c0712bfb1127bd",
+    "url": "/fazt-web/static/media/nginx.9bb21db0.svg"
+  },
+  {
+    "revision": "ccb43d3400fb05951c89ad3ba6acd9d5",
+    "url": "/fazt-web/static/media/nodejs.ccb43d34.svg"
+  },
+  {
+    "revision": "ac8e2c4148f043860c44068629f1cc27",
+    "url": "/fazt-web/static/media/nodewebkit.ac8e2c41.svg"
+  },
+  {
+    "revision": "28085fd3ef7a8afe957e92deb4e5a73c",
+    "url": "/fazt-web/static/media/notfound.28085fd3.svg"
+  },
+  {
+    "revision": "d8d8297d2b4db01f84834d39361f24b7",
+    "url": "/fazt-web/static/media/notfoundimg.d8d8297d.jpg"
+  },
+  {
+    "revision": "e83e60b310396562d703c9ccff616b91",
+    "url": "/fazt-web/static/media/npm.e83e60b3.svg"
+  },
+  {
+    "revision": "369bae91ff659183c379c49cc099660b",
+    "url": "/fazt-web/static/media/oracle.369bae91.svg"
+  },
+  {
+    "revision": "a799052fd1df6450741b2658ebfeac85",
+    "url": "/fazt-web/static/media/photoshop.a799052f.svg"
+  },
+  {
+    "revision": "7be7348ea07780291787cb3825fbf59f",
+    "url": "/fazt-web/static/media/php.7be7348e.svg"
+  },
+  {
+    "revision": "77caeb0d4249807e8d7389ee26cce6fd",
+    "url": "/fazt-web/static/media/phpstorm.77caeb0d.svg"
+  },
+  {
+    "revision": "9474528ec861c5d35b9efd498ddef326",
+    "url": "/fazt-web/static/media/postgresql.9474528e.svg"
+  },
+  {
+    "revision": "a297bd833416ec803d9ea62eaed65e43",
+    "url": "/fazt-web/static/media/protractor.a297bd83.svg"
+  },
+  {
+    "revision": "c65b5a946a3b31ee10d467fd78f56c45",
+    "url": "/fazt-web/static/media/pycharm.c65b5a94.svg"
+  },
+  {
+    "revision": "3b84eb4026f0c704e7e3d16972bb0e3d",
+    "url": "/fazt-web/static/media/python.3b84eb40.svg"
+  },
+  {
+    "revision": "cdbc91a7af04e1f040c17ab1f429e7df",
+    "url": "/fazt-web/static/media/rails.cdbc91a7.svg"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/fazt-web/static/media/react.e9f3d01c.svg"
+  },
+  {
+    "revision": "079e9835dc9457a772dd4e3d98fa1f75",
+    "url": "/fazt-web/static/media/redis.079e9835.svg"
+  },
+  {
+    "revision": "b7dc3b99382465652bda0e61515c0937",
+    "url": "/fazt-web/static/media/redux.b7dc3b99.svg"
+  },
+  {
+    "revision": "a3fdc39e66bd292b0300b392e7dbe4f8",
+    "url": "/fazt-web/static/media/ruby.a3fdc39e.svg"
+  },
+  {
+    "revision": "f704b843540e5985728eafb1bd6f2b19",
+    "url": "/fazt-web/static/media/rubymine.f704b843.svg"
+  },
+  {
+    "revision": "77fbc323a5bc35cbb8ea2e8c9b6e4642",
+    "url": "/fazt-web/static/media/rust.77fbc323.svg"
+  },
+  {
+    "revision": "88c1047792044b547d1d3e066d3e18c4",
+    "url": "/fazt-web/static/media/sass.88c10477.svg"
+  },
+  {
+    "revision": "cb55329bce9949b82635b6dfce1a916a",
+    "url": "/fazt-web/static/media/scala.cb55329b.svg"
+  },
+  {
+    "revision": "b8a803afc78c1e8a9a8edd77cd2b5c0f",
+    "url": "/fazt-web/static/media/sequelize.b8a803af.svg"
+  },
+  {
+    "revision": "9be5eb4a1357dff3f5f77bebf389fbef",
+    "url": "/fazt-web/static/media/sketch.9be5eb4a.svg"
+  },
+  {
+    "revision": "bd4a3d9256994fb686702e51ebec4816",
+    "url": "/fazt-web/static/media/slack.bd4a3d92.svg"
+  },
+  {
+    "revision": "1c14fb48a8fd9e91d59d3de82882b0e1",
+    "url": "/fazt-web/static/media/ssh.1c14fb48.svg"
+  },
+  {
+    "revision": "e68c487be93133788f116498db61df7a",
+    "url": "/fazt-web/static/media/stylus.e68c487b.svg"
+  },
+  {
+    "revision": "3f36cb0243e3952dae2675338749991c",
+    "url": "/fazt-web/static/media/swift.3f36cb02.svg"
+  },
+  {
+    "revision": "6d5db8b4222fa6748849490420f81021",
+    "url": "/fazt-web/static/media/symfony.6d5db8b4.svg"
+  },
+  {
+    "revision": "3506d4077cb7d5f836415d09e358f6b0",
+    "url": "/fazt-web/static/media/tomcat.3506d407.svg"
+  },
+  {
+    "revision": "a91f1cb2589605efcb97d52e7aca1387",
+    "url": "/fazt-web/static/media/travis.a91f1cb2.svg"
+  },
+  {
+    "revision": "3b32fc852ef80352a5fba60c45e4a53e",
+    "url": "/fazt-web/static/media/twitter.3b32fc85.svg"
+  },
+  {
+    "revision": "02867dbe5f804e4dbd0db076bb271cc4",
+    "url": "/fazt-web/static/media/typescript.02867dbe.svg"
+  },
+  {
+    "revision": "d2be82794cda1e40e7b53b56ea99fd17",
+    "url": "/fazt-web/static/media/vagrant.d2be8279.svg"
+  },
+  {
+    "revision": "adbb6ec992c2c60344794f9148fe205e",
+    "url": "/fazt-web/static/media/vim.adbb6ec9.svg"
+  },
+  {
+    "revision": "9cf6b38f4a8f45104d392f15c308e39e",
+    "url": "/fazt-web/static/media/vuejs.9cf6b38f.svg"
+  },
+  {
+    "revision": "9a7a86a89b95d03d85686fad1578790e",
+    "url": "/fazt-web/static/media/webpack.9a7a86a8.svg"
+  },
+  {
+    "revision": "a75c9899b1e6c8b48ffb7e324f2b1a9d",
+    "url": "/fazt-web/static/media/yarn.a75c9899.svg"
+  },
+  {
+    "revision": "f810300d346bd50baa7d181417bb90eb",
+    "url": "/fazt-web/static/media/yii.f810300d.svg"
+  },
+  {
+    "revision": "04f2b66c588e6f3920a676bc464d48f3",
+    "url": "/fazt-web/static/media/zend.04f2b66c.svg"
+  }
+]);
